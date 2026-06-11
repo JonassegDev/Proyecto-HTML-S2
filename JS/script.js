bc14a7db5092734340a2a3569b2220c9388bcc34
Jonas Espinoza <jonassegdev@gmail.com>
2026-06-10 21:50:41 -0600

    Cambios

---
+++ JS/script.js
@@ -0,0 +1,22 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    if (hamburger && navMenu) {
+        hamburger.addEventListener('click', () => {
+            navMenu.classList.toggle('active');
+            
+            // Cambiar ícono de barras a X
+            const icon = hamburger.querySelector('i');
+            if (navMenu.classList.contains('active')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-xmark');
+            } else {
+                icon.classList.remove('fa-xmark');
+                icon.classList.add('fa-bars');
+            }
+        });
+    }
+});
